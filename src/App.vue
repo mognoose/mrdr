cc8d0776fc5995d5c9be74d26a40b2976659a5d8
Csaba <csaba@lamia.fi>
2022-12-09 15:43:42 +0200

    add variables to styles

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,23 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Cuprum:wght@400;700&display=swap');
 
+$fg-primary: rgb(220,200,150);
+$fg-secondary: rgba(220,200,150,.5);
+$fg-light: rgb(250, 235, 215);
+
+$bg-primary: rgb(55,55,55);
+$bg-secondary: rgba(220,200,150,0.1);
+$bg-light: rgba(220,200,150,0.4);
+
+$shadow: rgba(0,0,0,.5);
+$dark: rgb(40,40,40);
+$highlight: rgba(225,200,125,.5);
+
 html,
 body {
   margin: 0;
   padding: 0;
-  background-color: rgb(55,55,55);
+  background-color: $bg-primary;
   font-family: 'Cuprum', sans-serif;
 }
 
@@ -20,25 +32,13 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: rgb(220,200,150);
+  color: $fg-primary;
   background-image: url('~@/assets/bg.jpg');
   background-size: cover;
   background-position: center;
   height: 100vh;
 }
 
-// nav {
-//   padding: 30px;
-
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
 .form-group {
   margin: 1em;
   display: grid;
@@ -56,9 +56,9 @@ button,
 input {
   margin: 2px;
   padding: .5em;
-  border: 2px solid rgb(220,200,150);
-  color:rgb(220,200,150);
-  background-color: rgb(55,55,55);
+  border: 2px solid $fg-primary;
+  color:$fg-primary;
+  background-color: $bg-primary;
   border-radius: 5px;
   font-family: 'Cuprum', sans-serif;
   text-transform: uppercase;
@@ -68,25 +68,35 @@ input {
 button {
   cursor: pointer;
   text-transform: uppercase;
-  background-color: rgba(220,200,150,0.1);
+  background-color: $bg-secondary;
+  box-shadow: 0 0 10px $shadow;
+  transition: 200ms;
 }
 
 button:hover{
-  background-color: rgba(220,200,150,0.4);
-  color:rgb(250, 235, 215);
+  background-color: $bg-light;
+  color: $fg-light;
+  box-shadow: 0 0 10px $highlight
 }
 
 input {
   margin: 1em;
-  background-color: rgb(40,40,40);
+  background-color: $dark;
   border-radius: 0px;
   border-top: 0px;
   border-left: 0px;
   border-right: 0px;
+  transition: 200ms;
+
+}
+
+input:hover {
+  box-shadow: 0 0 10px $shadow;
 }
 
 input:focus {
   outline-width: 0;
+  box-shadow: 0 0 10px $bg-secondary
 }
 
 table{
@@ -94,12 +104,12 @@ table{
 }
 
 th{
-  border-bottom: 2px solid rgb(220,200,150);
+  border-bottom: 2px solid $fg-primary;
   padding: .25em 1em;
 }
 
 td{
-  border-bottom: 1px solid rgba(220,200,150,.5);
+  border-bottom: 1px solid $fg-secondary;
   padding: .25em 1em;
 }
 </style>
